eb26198e3b3f89bf966c1dc972f24b75f3579db8
Jose Garza <15210549+joselgarza@users.noreply.github.com>
2023-11-30 04:42:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// Create web server
+// Import http module
+const http = require('http');
+
+// Define the server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+// Define the listening port
+const port = 3000;
+
+// Start the server
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
