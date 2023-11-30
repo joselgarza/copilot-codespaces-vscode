30fc982e5eb78fe6907be64fa87144edcc2f96e4
Jose Garza <15210549+joselgarza@users.noreply.github.com>
2023-11-30 04:37:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+}
